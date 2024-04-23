2c4ffa6bc41c02c9e80274a304c210da2fecd3d2
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-23 16:28:02 +0000

    Getting ARM object

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import msal
 import os
+import requests
 
 from azure.identity import ManagedIdentityCredential
 from dotenv import load_dotenv
@@ -10,6 +11,12 @@ spn_client_id = os.getenv("SPN_CLIENT_ID")
 spn_client_secret = os.getenv("SPN_CLIENT_SECRET")
 uami_client_id = os.getenv("UAMI_CLIENT_ID")
 tenant_id = os.getenv("TENANT_ID")
+subscription_id = os.getenv("SUBSCRIPTION_ID")
+resource_group = os.getenv("RESOURCE_GROUP")
+resource_provider = os.getenv("RESOURCE_PROVIDER")
+resource_type =  os.getenv("RESOURCE_TYPE")
+resource_name = os.getenv("RESOURCE_NAME")
+resource_provider_api_version = os.getenv("RESOURCE_PROVIDER_API_VERSION")
 
 scopes = ["https://management.azure.com/.default"]
 
@@ -41,3 +48,14 @@ uami_token = uami_credential.get_token(*scopes)
 
 print("Token from azure.identity for UAMI")
 print(uami_token.token)
+
+# Getting ARM object
+#
+resource_uri = f"subscriptions/{subscription_id}/resourcegroups/{resource_group}/providers/{resource_provider}/{resource_type}/{resource_name}"
+full_url = f"https://management.azure.com/{resource_uri}?api-version={resource_provider_api_version}"
+
+print(f"Getting ARM object from {full_url}")
+
+headers = {'Authorization': 'Bearer {}'.format(uami_token.token)}
+response = requests.request("GET", full_url, headers=headers)
+print(response.json())
