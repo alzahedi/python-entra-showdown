dbd9e642a3a325f4ec26d61283e568b54817a937
Alfurquan Zahedi <alzahedi@microsoft.com>
2024-06-14 12:52:01 +0530

    Try out

--- decrypt-token-pre-req.ps1
+++ decrypt-token-pre-req.ps1
@@ -1,4 +1,19 @@
 
+param(
+    [string]
+    $SPN_CLIENT_ID,
+    [string]
+    $SPN_CLIENT_SECRET,
+    [string]
+    $SPN_TENANT_ID,
+    [string]
+    $SUBSCRIPTION_ID,
+    [string]
+    $RESOURCE_GROUP_NAME,
+    [string]
+    $LOCATION
+)
+
 try {
     choco config get cacheLocation
 }
@@ -28,3 +43,166 @@ foreach ($app in $appsToInstall) {
         & choco install $name --version $version  --params $params /y -Force | Write-Output
     }
 }
+
+# load env path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") 
+
+# login to azure
+az login --service-principal --username $SPN_CLIENT_ID --password $SPN_CLIENT_SECRET --tenant $SPN_TENANT_ID
+# set subscription
+az account set --subscription $SUBSCRIPTION_ID
+
+function downloadArcAgent() {
+    <#
+
+    .SYNOPSIS
+    Downloads the Azure Arc Connected Machine Agent from the Microsoft download site.
+
+    #>
+    $ProgressPreference="SilentlyContinue"; 
+    Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi
+}
+
+function getTokenFromArc(){
+    $apiVersion = "2020-06-01"
+    $resource = "https://management.azure.com/"
+    $endpoint = "{0}?resource={1}&api-version={2}" -f $env:IDENTITY_ENDPOINT,$resource,$apiVersion
+    $secretFile = ""
+    try
+    {
+        Invoke-WebRequest -Method GET -Uri $endpoint -Headers @{Metadata='True'} -UseBasicParsing
+    }
+    catch
+    {
+        $wwwAuthHeader = $_.Exception.Response.Headers["WWW-Authenticate"]
+        if ($wwwAuthHeader -match "Basic realm=.+")
+        {
+            $secretFile = ($wwwAuthHeader -split "Basic realm=")[1]
+        }
+    }
+    Write-Host "Secret file path: " $secretFile`n
+    $secret = cat -Raw $secretFile
+    $response = Invoke-WebRequest -Method GET -Uri $endpoint -Headers @{Metadata='True'; Authorization="Basic $secret"} -UseBasicParsing
+    if ($response)
+    {
+        $token = (ConvertFrom-Json -InputObject $response.Content).access_token
+        return $token
+    }
+    return $null
+}
+
+function Parse-JWTtoken {
+ 
+    [cmdletbinding()]
+    param([Parameter(Mandatory=$true)][string]$token)
+ 
+    #Validate as per https://tools.ietf.org/html/rfc7519
+    #Access and ID tokens are fine, Refresh tokens will not work
+    if (!$token.Contains(".") -or !$token.StartsWith("eyJ")) { Write-Error "Invalid token" -ErrorAction Stop }
+ 
+    #Header
+    $tokenheader = $token.Split(".")[0].Replace('-', '+').Replace('_', '/')
+    #Fix padding as needed, keep adding "=" until string length modulus 4 reaches 0
+    while ($tokenheader.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenheader += "=" }
+    Write-Verbose "Base64 encoded (padded) header:"
+    Write-Verbose $tokenheader
+    #Convert from Base64 encoded string to PSObject all at once
+    Write-Verbose "Decoded header:"
+    [System.Text.Encoding]::ASCII.GetString([system.convert]::FromBase64String($tokenheader)) | ConvertFrom-Json | fl | Out-Default
+ 
+    #Payload
+    $tokenPayload = $token.Split(".")[1].Replace('-', '+').Replace('_', '/')
+    #Fix padding as needed, keep adding "=" until string length modulus 4 reaches 0
+    while ($tokenPayload.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenPayload += "=" }
+    Write-Verbose "Base64 encoded (padded) payoad:"
+    Write-Verbose $tokenPayload
+    #Convert to Byte array
+    $tokenByteArray = [System.Convert]::FromBase64String($tokenPayload)
+    #Convert to string array
+    $tokenArray = [System.Text.Encoding]::ASCII.GetString($tokenByteArray)
+    Write-Verbose "Decoded array in JSON format:"
+    Write-Verbose $tokenArray
+    #Convert from JSON to PSObject
+    $tokobj = $tokenArray | ConvertFrom-Json
+    Write-Verbose "Decoded Payload:"
+    
+    return $tokobj
+}
+
+# Create Resource Group - idempotent
+#
+Write-Host "Creating resource group and adding skip Auto-onboarding tag, since we want to test additional permutations..."
+az group create --name $RESOURCE_GROUP_NAME `
+                --location $LOCATION `
+                --tags "ArcSQLServerExtensionDeployment=Disabled"
+
+
+# Download and install the Azure Arc Agent                
+Write-Host "Downloading Arc Agent..."
+downloadArcAgent
+
+Write-Host "Starting Arc Agent installation..."
+$exitCode = (Start-Process -FilePath msiexec.exe -ArgumentList @("/i", "AzureConnectedMachineAgent.msi" ,"/l*v", "installationlog.txt", "/qn") -Wait -Passthru).ExitCode
+
+if($exitCode -ne 0) {
+    $message=(net helpmsg $exitCode)
+    throw "Installation failed: $message See installationlog.txt for additional details."
+}
+
+$vmName = hostname
+
+# Run connect command
+#
+& "$Env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect `
+--service-principal-id $SPN_CLIENT_ID `
+--service-principal-secret $SPN_CLIENT_SECRET `
+--resource-group $RESOURCE_GROUP_NAME `
+--tenant-id $TENANT_ID `
+--location $LOCATION `
+--subscription-id $SUBSCRIPTION_ID `
+--resource-name $vmName `
+--cloud "AzureCloud" `
+--tags "Project=arceedata" `
+--correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a" # Do no change!
+
+if($LastExitCode -eq 0){Write-Host -ForegroundColor yellow "To view your onboarded server(s), navigate to https://ms.portal.azure.com/#blade/HubsExtension/BrowseResource/resourceType/Microsoft.HybridCompute%2Fmachines"}
+
+Write-Host "Arc Agent Installation complete."
+
+# Get token from arc
+$token = getTokenFromArc
+$parsedToken = Parse-JWTtoken -token $token
+$appId = $parsedToken.appid
+
+# Go ahead and replace spn client id with app id
+$userDirectory = $env:USERPROFILE
+Write-Host "User directory is $userDirectory"
+
+Push-Location $userDirectory\.azure
+
+(Get-Content azureProfile.json).Replace($SPN_CLIENT_ID, $appId) | Set-Content azureProfile.json
+
+Pop-Location
+
+$GIT_ROOT = git rev-parse --show-toplevel
+cd "$GIT_ROOT"
+
+New-Item .env | Out-Null
+
+$content = "TENANT_ID=$TENANT_ID"
+
+Add-Content ".env" $content
+
+python -m venv venv
+
+.\venv\Scripts\Activate.ps1
+
+pip install -r requirements.txt
+
+Write-Host "Pre-requisites installed successfully"
+
+python .\decrypt-token-store.py
+
+Write-Host "Token decrypted successfully"
+
+
